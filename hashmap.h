b1c67958f06bd9be36d016d54edfea3e04876d28
sinyovercosy <yan.tangent@gmail.com>
2024-12-17 06:42:36 +0000

    change insert behavior

--- hashmap.h
+++ hashmap.h
@@ -5,7 +5,7 @@
 
 namespace ty {
 
-template <typename K, typename V, typename Hash = std::hash<K>, typename Allocator = std::allocator<std::pair<const K, V>>>
+template <typename K, typename V, typename Hash = std::hash<K>, typename Eq = std::equal_to<K>, typename Allocator = std::allocator<std::pair<const K, V>>>
   requires std::equality_comparable<K> && std::is_nothrow_invocable_r_v<std::size_t, Hash, K> && std::is_move_constructible_v<std::pair<const K, V>>
 class flat_hash_map_lite {
  public:
@@ -16,30 +16,28 @@ class flat_hash_map_lite {
   // TODO: 1) return an iterator, and 2) add const version
   value_type* find(K const& key) {
     if (empty()) {
-      return nullptr;  // special case where Capacity() == 1 but we have not allocated anything
+      return nullptr;  // special case where capacity_ == 1 but we have not allocated anything
     }
-    ProbeSeq seq;
-    const std::size_t origin = Clamp(Hash{}(key), capacity_);
-    std::size_t i = origin;
-    while (ctrl_[i]) {
-      if (data_[i].first == key) {
-        return std::addressof(data_[i]);
-      } else {
-        i = Clamp(origin + seq.NextOffset(), capacity_);
-      }
-    }
-    return nullptr;
+    std::size_t i = FindIndex(key);
+    return ctrl_[i] ? std::addressof(data_[i]) : nullptr;
   }
 
-  void insert(value_type kv) {
-    size_++;
-    if (size_ >= ResizeThreshold()) {
-      Rehash();
+  /**
+   * inserts a new element iff the key does not already exist in the map
+   * @returns: (pointer to the inserted element or an existing element with equal key, a boolean indicating if insertion took place)
+   */
+  std::pair<value_type*, bool> insert(value_type kv) {
+    if (size_ + 1 >= capacity_) {
+      Rehash();  // assume that we are likely inserting a new element
     }
-    assert(size_ < capacity_);  // always exists a free slot
-    std::size_t i = ProbeNextFreeSlot(kv.first, ctrl_);
+    std::size_t i = FindIndex(kv.first);
+    if (ctrl_[i]) {
+      return {std::addressof(data_[i]), false};
+    }
+    size_++;
     ctrl_[i] = true;
     std::construct_at(std::addressof(data_[i]), std::move(kv));
+    return {std::addressof(data_[i]), true};
   }
 
  private:
@@ -58,24 +56,35 @@ class flat_hash_map_lite {
     std::uint32_t i = 0;
   };
 
-  std::size_t ProbeNextFreeSlot(K const& key, std::vector<bool, Allocator> const& not_free) {
-    const std::size_t origin = Clamp(Hash{}(key), not_free.size());
+  std::size_t FindIndex(K const& key) {
+    assert(capacity_ > 0);
+    const std::size_t origin = Clamp(Hash{}(key), capacity_);
     ProbeSeq seq;
     std::size_t i = origin;
-    while (not_free[i]) {
-      i = Clamp(origin + seq.NextOffset(), not_free.size());
+    while (ctrl_[i]) {
+      if (data_[i].first == key) {
+        return i;
+      } else {
+        i = Clamp(origin + seq.NextOffset(), capacity_);
+      }
     }
     return i;
   }
 
   void Rehash() {
-    std::vector<bool, Allocator> new_ctrl(capacity_ * 2, allocator_);
-    value_type* new_data = allocator_.allocate(capacity_ * 2);
+    const std::size_t new_capacity = capacity_ * 2;
+    std::vector<bool, Allocator> new_ctrl(new_capacity, allocator_);
+    value_type* new_data = allocator_.allocate(new_capacity);
     for (std::size_t i = 0; i < ctrl_.size(); ++i) {
       if (!ctrl_[i]) {
         continue;
       }
-      std::size_t new_i = ProbeNextFreeSlot(data_[i].first, new_ctrl);
+      const std::size_t origin = Clamp(Hash{}(data_[i].first), new_capacity);
+      ProbeSeq seq;
+      std::size_t new_i = origin;
+      while (new_ctrl[new_i]) {
+        new_i = Clamp(origin + seq.NextOffset(), new_capacity);
+      }
       new_ctrl[new_i] = true;
       std::construct_at(std::addressof(new_data[new_i]), std::move(data_[i]));
     }
--- hashmap_test.cc
+++ hashmap_test.cc
@@ -1,10 +1,12 @@
 #include "hashmap.h"
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "gmock/gmock.h"
 
 namespace ty {
 namespace {
 
-TEST(MapTest, TestBasic) {
+TEST(MapTest, TestFind) {
   flat_hash_map_lite<std::string, int> map;
   EXPECT_FALSE(map.find("abc"));
   map.insert({"abc", 123});
@@ -12,6 +14,19 @@ TEST(MapTest, TestBasic) {
   EXPECT_TRUE(result != nullptr);
   EXPECT_EQ(result->second, 123);
   EXPECT_FALSE(map.find("def"));
+}
+
+TEST(MapTest, TestInsert) {
+  flat_hash_map_lite<std::string, int> map;
+  auto result_1 = map.insert({"abc", 123});
+  EXPECT_THAT(result_1, ::testing::Pair(::testing::NotNull(), true));
+  EXPECT_THAT(*result_1.first, ::testing::Pair("abc", 123));
+  auto result_2 = map.insert({"abc", 456});
+  EXPECT_THAT(result_2, ::testing::Pair(::testing::NotNull(), false));
+  EXPECT_THAT(*result_2.first, ::testing::Pair("abc", 123));
+  auto result_3 = map.insert({"def", 123});
+  EXPECT_THAT(result_3, ::testing::Pair(testing::Not(result_1.first), true));
+  EXPECT_THAT(*result_3.first, ::testing::Pair("def", 123));
 }
 
 constexpr auto very_bad_hash = [](std::string const&) noexcept -> std::size_t { return 0; };
